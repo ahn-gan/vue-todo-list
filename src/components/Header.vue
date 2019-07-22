b6ea9a87e4dede1dd116ed7b82263cdf25702bff
Akin <akin.gan@oocl.com>
2019-07-22 19:34:32 +0800

    fix: separate the Header Information to the Header component.

---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <el-row>
+    <el-row>
+      <h2>Vue Todo List</h2>
+    </el-row>
+    <el-row>
+      <em>Simple Todo List with adding and filter by diff status.</em>
+    </el-row>
+  </el-row>
+</template>
+
+<script>
+    export default {
+        name: "Header"
+    }
+</script>
+
+<style scoped>
+
+</style>
